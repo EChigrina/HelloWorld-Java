19241dd1c1b39538dc4c4ecc079af1f43ffc8f10
EChigrina <elizaveta.chigrina@codeinside.ru>
2019-07-17 18:37:20 +0300

    webhooks controller

--- src/main/java/com/intuit/developer/helloworld/controller/WebhooksController.java
+++ src/main/java/com/intuit/developer/helloworld/controller/WebhooksController.java
@@ -1,21 +1,26 @@
 package com.intuit.developer.helloworld.controller;
 
 import com.intuit.developer.helloworld.client.OAuth2PlatformClientFactory;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class WebhooksController {
@@ -35,7 +40,7 @@ public class WebhooksController {
     @PostMapping("/webhooks")
     //@ResponseBody
     public void webhooks(@RequestHeader(SIGNATURE) String signature, @RequestBody String payload, Model model) {
-        HttpHeaders headers = new HttpHeaders();
+       /* HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
 
         MultiValueMap<String, String> map= new LinkedMultiValueMap<String, String>();
@@ -45,6 +50,23 @@ public class WebhooksController {
 
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<String> response = restTemplate.postForEntity( "https://dxfeed-quickbooksintegration.cs17.force.com/services/apexrest/SalesforceQuickbooksIntegration", request , String.class );
-        //logger.info(response);
+        //logger.info(response);*/
+        HttpClient httpclient = HttpClients.createDefault();
+        HttpPost httppost = new HttpPost("https://dxfeed-quickbooksintegration.cs17.force.com/services/apexrest/SalesforceQuickbooksIntegration");
+
+// Request parameters and other properties.
+        List<NameValuePair> params = new ArrayList<NameValuePair>(2);
+        params.add(new BasicNameValuePair("payload", payload));
+        try {
+            httppost.setEntity(new UrlEncodedFormEntity(params, "UTF-8"));
+            httppost.setHeader("Content-Type", "application/json; charset=UTF-8");
+
+//Execute and get the response.
+            HttpResponse response = httpclient.execute(httppost);
+            HttpEntity entity = response.getEntity();
+        }
+        catch(IOException e) {
+
+        }
     }
 }
